3632ed3ad64de0adec9af8bc7b2647856d93b891
wanghui1 <wanghui1@enovatemotors.com>
2020-12-09 19:29:02 +0800

    AndroidDemo: add shape usage
    Signed-off-by: wanghui1 <wanghui1@enovatemotors.com>

--- junior/src/main/java/com/example/junior/ShapeActivity.java
+++ junior/src/main/java/com/example/junior/ShapeActivity.java
@@ -16,20 +16,26 @@ public class ShapeActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_shape);
         //从布局文件中获取名叫v_content的视图
         v_content = findViewById(R.id.v_content);
-        //给btn_rect设置点击监听器
         findViewById(R.id.btn_rect).setOnClickListener(this);
-        //给btn_oval设置点击监听器
         findViewById(R.id.btn_oval).setOnClickListener(this);
+        findViewById(R.id.btn_ring).setOnClickListener(this);
+        findViewById(R.id.btn_dash).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_rect) { //点击了按钮btn_rect
             //把矩形形状设置为v_content的背景
-            v_content.setBackgroundResource(R.drawable.shape_rect_gold);
+            v_content.setBackgroundResource(R.drawable.shape_rect);
         } else if (v.getId() == R.id.btn_oval) { //点击了按钮btn_oval
             //把椭圆形状设置为v_content的背景
-            v_content.setBackgroundResource(R.drawable.shape_oval_rose);
+            v_content.setBackgroundResource(R.drawable.shape_oval);
+        } else if (v.getId() == R.id.btn_ring) { //点击了按钮btn_oval
+            //把椭圆形状设置为v_content的背景
+            v_content.setBackgroundResource(R.drawable.shape_ring);
+        } else if (v.getId() == R.id.btn_dash) { //点击了按钮btn_oval
+            //把椭圆形状设置为v_content的背景
+            v_content.setBackgroundResource(R.drawable.shape_dash);
         }
     }
 
