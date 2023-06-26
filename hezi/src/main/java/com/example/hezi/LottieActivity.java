ceb391e87730878535d40a3341047e5f8c48511e
wanghui <wanghui1992cx@163.com>
2023-06-26 23:15:26 +0800

    AndroidDemo: add hezi project
    Signed-off-by: wanghui <wanghui1992cx@163.com>

--- hezi/src/main/java/com/example/hezi/LottieActivity.java
+++ hezi/src/main/java/com/example/hezi/LottieActivity.java
@@ -16,6 +16,7 @@ import com.airbnb.lottie.LottieDrawable;
 public class LottieActivity extends AppCompatActivity {
 
     private LottieAnimationView mLottieAnimationView;
+    private SpeedTextView mSpeedTextView;
 
     @SuppressLint("WrongConstant")
     @Override
@@ -23,9 +24,13 @@ public class LottieActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lottie);
         mLottieAnimationView = findViewById(R.id.lottie_view);
+        mSpeedTextView = findViewById(R.id.speed_view);
+
         mLottieAnimationView.setAnimation("data.json");
         mLottieAnimationView.setRepeatMode(LottieDrawable.RESTART);
         mLottieAnimationView.setRepeatCount(LottieDrawable.INFINITE);
         mLottieAnimationView.playAnimation();
+
+        mSpeedTextView.startAnim();
     }
 }
---
+++ hezi/src/main/java/com/example/hezi/SpeedTextView.java
@@ -0,0 +1,40 @@
+package com.example.hezi;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.animation.LinearInterpolator;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class SpeedTextView extends AppCompatTextView {
+
+    private static final String TAG = "SpeedTextView";
+
+    public SpeedTextView(Context context) {
+        this(context, null);
+    }
+
+    public SpeedTextView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SpeedTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void startAnim() {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+        valueAnimator.addUpdateListener(valueAnimator1 -> {
+            Log.i(TAG, "onAnimationUpdate: " + valueAnimator1.getAnimatedValue());
+            setText(String.valueOf(valueAnimator1.getAnimatedValue()));
+        });
+        valueAnimator.setDuration(1000);
+        valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.start();
+    }
+}
